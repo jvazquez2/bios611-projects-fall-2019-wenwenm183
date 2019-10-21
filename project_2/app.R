25d923b0664098d266742d630029724bab10692a
Wenwen Mei <mwen@live.unc.edu>
2019-10-21 18:04:28 -0400

    project_2

--- project_2/app.R
+++ project_2/app.R
@@ -1,1 +1,55 @@
+library(shiny)
+library(tidyverse)
+source("helper_functions.R", local = F)
 
+# Define UI for slider demo app ----
+ui <- fluidPage(
+  
+  # App title ----
+  titlePanel("Project_2"),
+  
+  # Sidebar layout with input and output definitions ----
+  sidebarLayout(
+    
+    # Sidebar to demonstrate various slider options ----
+    sidebarPanel(
+      
+      
+      selectInput("select", label = h3("Select a variable"),
+                  choices = list("Bus" = "Bus", "Food" = "Food", "Cloth" = "Cloth", "School"="School", "Hygiene"="Hygiene"), 
+                  selected = 1),
+      
+      
+      #output 
+      fluidRow(column(width=12, verbatimTextOutput("value")))
+    ),
+    
+    # Main panel for displaying outputs ----
+    mainPanel(
+      
+      # Output: plot for each of the variables ----
+      plotOutput(outputId = "popPlot")
+      
+    )
+  )
+)
+
+
+
+server <- function(input, output) {
+  
+  # 
+  output$value <- renderPrint({ 
+    paste("You have selected", input$select)
+  })
+  
+  
+  output$popPlot <- renderPlot({
+    plot(input$select)
+    
+  })
+  
+}
+
+# Create Shiny app ----
+shinyApp(ui, server)
--- project_2/helper_functions.R
+++ project_2/helper_functions.R
@@ -1,1 +1,59 @@
+library(ggplot2)
+library(tidyverse)
+library(psych)
+require(data.table)
+
+#filter the datset 
+data<-as.data.frame(fread("./data/UMD_Services_Provided_20190719.tsv")) %>%
+  subset(select=c(1, 2, 4,6, 7, 8, 9, 10, 11))
+data$Date <- as.Date(data$Date, "%m/%d/%Y") 
+
+data1 <- data %>% 
+  separate(Date, sep="-", into = c("Year", "Month", "Day")) %>%
+  filter(1999<= Year & Year <= 2018)
+
+data_Bus <- data1 %>%
+  drop_na('Bus Tickets (Number of)') %>%
+  filter('Bus Tickets (Number of)' > 0) %>%
+  group_by(Year, Month) %>%
+  summarise(count = n(),
+            count= sum(`Bus Tickets (Number of)`))
+
+data_Food <- data1 %>%
+  drop_na('Food Provided for') %>%
+  filter('Food Provided for' > 0) %>%
+  group_by(Year, Month) %>%
+  summarise(count = n(),
+            count= sum(`Food Provided for`))
+
+data_Cloth <- data1 %>%
+  drop_na('Clothing Items') %>%
+  filter('Clothing Items' > 0) %>%
+  group_by(Year, Month) %>%
+  summarise(count = n(),
+            count= sum(`Clothing Items`))
+
+data_School <-data1 %>%
+  drop_na('School Kits') %>%
+  filter('School Kits' > 0) %>%
+  group_by(Year, Month) %>%
+  summarise(count = n(),
+            count= sum(`School Kits`))
+
+data_Hygiene <- data1 %>%
+  drop_na('Hygiene Kits') %>%
+  filter('Hygiene Kits' > 0) %>%
+  group_by(Year, Month) %>%
+  summarise(count = n(),
+            count= sum(`Hygiene Kits`))
+
+
+
+# plot
+plot <- function(a) {
+  c = get(paste("data",a,sep = "_"))
+  ggplot(c,aes(Month, Year, fill=count)) + geom_tile()+ theme_classic() +
+    labs(title=paste('Number of Clients Claimed',a, 'Products From UMD'))
+}
+
 
